c5ff83b9c99e10b35426e60576c3963c90c934e5
arsanto <arsanto@users.noreply.github.com>
2016-02-08 21:02:52 +0700

    Create enter.sh

---
+++ enter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run: source <(curl -s https://github.com/arsanto/soXauto/blob/master/install.sh
+# sox auto
+
+ffmpeg -loop 1 -framerate 2 -i filename.jpg -i filename.mp3 -c:v libx264 -preset fast -tune stillimage -crf 18 -c:a copy -shortest -pix_fmt yuv420p out.mkv
+
+ffmpeg -i out.mkv -filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" output.mkv
+
+youtube-upload --title="1.3" output.mkv
+
+rm -f filename.jpg
+rm -f filename.mp3
+rm -f output.mkv
+
